22bcd1518fa1cf33dffa1056a1b6fd8a52777d65
Joshua Hochuli <joshua_hochuli@unc.edu>
2018-12-19 16:10:32 -0500

    heatmaps implemented unified output structure timestamps handled (although manually input)

--- plot_traces.py
+++ plot_traces.py
@@ -1,89 +1,190 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 def main():
 
-    plt.rcParams.update({'font.size':6})
+    timestamp = "1545167676"
+
     connections = [True,False]
     targets = ['PY', 'HTC', 'RTC', 'FS', 'IN', 'RE', 'TC']
     #targets = ['PY']
     left_options = [True,False]
 
+    heatmaps(targets, connections, left_options, timestamp)
+    voltage_traces(targets, connections, left_options, timestamp)
+
+def get_output_stem(timestamp):
+
+    return "output/figures/" + timestamp
+
+def heatmaps(targets, connections, left_options, timestamp):
+
+    for target in targets:
+        for connected in connections:
+            for left in left_options:
+
+                fig = plt.figure(figsize = (11, 8.5))
+
+                stem = get_filename_stem(target, timestamp, connected, left)
+                volt_filename = stem + "volt.npy"
+
+                data = np.load(volt_filename)
+
+                #normalize plot height
+                num_neurons = data.shape[0]
+                aspect = 20000 / num_neurons
+
+                #determine maximum magnitude for centering colormap around 0
+                m = 0
+                for x in data.flat:
+                    if abs(x) > m:
+                        m = abs(x)
+
+                plt.imshow(data, aspect = aspect, cmap = 'seismic', vmin = -m, vmax = m)
+                plt.colorbar(shrink = 0.35)
+                plt.xlabel('Time')
+                plt.ylabel('Neuron index')
+
+                output_dir = get_output_stem(timestamp) + "/heatmaps/"
+                if(connected):
+                    output_dir = output_dir + "connected/"
+                else:
+                    output_dir = output_dir + "unconnected/"
+
+                if(left):
+                    output_dir = output_dir + "left/"
+                else:
+                    output_dir = output_dir + "right/"
+
+                os.makedirs(output_dir, exist_ok = True)
+
+                output_filename = output_dir + target
+
+                if(connected):
+                    output_filename = output_filename + "_connected"
+                else:
+                    output_filename = output_filename + "_unconnected"
+                if(left):
+                    output_filename = output_filename + "_left"
+                else:
+                    output_filename = output_filename + "_right"
+
+                output_filename = output_filename + "_heatmap.pdf"
+                print(output_filename)
+                fig.savefig(output_filename)
+                plt.close()
+
+
+
+
+
+
+def get_filename_stem(target, timestamp, connected, is_left):
+
+    if(connected):
+        stem = "output/" + timestamp + "/connected/"
+    else:
+        stem = "output/" + timestamp + "/unconnected/"
+
+    if(is_left):
+        name = target + "L_"
+    else:
+        name = target + "R_"
+
+    return stem + name
+
+
+
+def voltage_traces(targets, connections, left_options, timestamp, average = False):
+
+    plt.rcParams.update({'font.size':6})
+
     for target in targets:
 
-        plot(target, connections, left_options)
+        nrows = len(connections)
+        ncols = len(left_options)
 
-def plot(target, connections, left_options):
+        f, axarr = plt.subplots(2,2)
 
-    nrows = len(connections)
-    ncols = len(left_options)
+        subplot_counter = 1
+        i = 0
+        for connected in connections:
+            j = 0
 
-    f, axarr = plt.subplots(2,2)
+            for left in left_options:
 
-    subplot_counter = 1
-    i = 0
-    for connected in connections:
-        j = 0
+                ax = axarr[i,j]
+                stem = get_filename_stem(target, timestamp, connected, left)
+                time_filename = stem + "time.npy"
+                volt_filename = stem + "volt.npy"
 
-        for left in left_options:
+                time = np.load(time_filename)
+                volt = np.load(volt_filename)
 
-            ax = axarr[i,j]
+                subplot_code = (100 * nrows) + (10 * ncols) + subplot_counter
 
-            if(connected):
-                stem = "output/connected/"
-            else:
-                stem = "output/unconnected/"
+                if average:
 
-            if(left):
-                name = target + "L_"
-            else:
-                name = target + "R_"
+                    num_neurons = volt.shape[0]
+                    num_points = volt.shape[1]
+                    values = np.empty(shape = (volt.shape[1]))
+                    for k in range(num_points):
 
-            time_file = name + "time.npy"
-            volt_file = name + "volt.npy"
+                        s = 0
+                        for l in range(num_neurons):
+                            s = s + volt[l,k]
+                        values[k] = s / num_neurons
 
-            time = np.load(stem + time_file)
-            volt = np.load(stem + volt_file)
 
-            subplot_code = (100 * nrows) + (10 * ncols) + subplot_counter
+                    ax.plot(time, values, linewidth = 0.5, color = "#3333cc")
 
-            for x in range(volt.shape[0]):
-                ax.plot(time, volt[x], linewidth = 0.5, color = "#3333cc",
-                        alpha = 0.5)
-
-            xlabel = "Time (seconds)"
-            ylabel = "Voltage (volts)"
-
-            ax.set(xlabel = xlabel, ylabel = ylabel)
-
-            if(j == 0):
-                if(connected):
-                    label = "Connected"
                 else:
-                    label = "Not Connected"
-                ax.annotate(label, xy=(-0.65,0.5), xycoords=("axes fraction",
-                    "axes fraction"), weight = "bold")
+                    for x in range(volt.shape[0]):
+                        ax.plot(time, volt[x], linewidth = 0.5, color = "#3333cc",
+                                alpha = 0.5)
 
-            if(i == 0):
-                if(left):
-                    label = "Left"
-                else:
-                    label = "Right"
+                xlabel = "Time (seconds)"
+                ylabel = "Voltage (volts)"
 
-                ax.annotate(label, xy=(0.45,1.1), xycoords=("axes fraction",
-                    "axes fraction"), weight = "bold")
-            subplot_counter = subplot_counter + 1
-            j = j + 1
-        i = i + 1
+                ax.set(xlabel = xlabel, ylabel = ylabel)
 
-    f.tight_layout(rect=[0.15,0,1,0.9])
-    f.suptitle(target, x = 0.6, fontsize = 15)
+                if(j == 0):
+                    if(connected):
+                        label = "Connected"
+                    else:
+                        label = "Not Connected"
+                    ax.annotate(label, xy=(-0.65,0.5), xycoords=("axes fraction",
+                        "axes fraction"), weight = "bold")
 
-    output_dir = "output/figures/run_2/"
-    filename = output_dir + target + ".png"
-    print(filename)
-    f.savefig(filename, pad_inches = 10)
+                if(i == 0):
+                    if(left):
+                        label = "Left"
+                    else:
+                        label = "Right"
+
+                    ax.annotate(label, xy=(0.45,1.1), xycoords=("axes fraction",
+                        "axes fraction"), weight = "bold")
+                subplot_counter = subplot_counter + 1
+                j = j + 1
+            i = i + 1
+
+        f.tight_layout(rect=[0.15,0,1,0.9])
+
+        title = target
+
+        if(average):
+            title = title + " (Average across %d neurons)" % volt.shape[0]
+
+        f.suptitle(title, x = 0.6, fontsize = 15)
+
+        output_dir = get_output_stem(timestamp) + "/voltage_traces/"
+        os.makedirs(output_dir, exist_ok = True)
+
+        filename = output_dir + target + "_voltage_trace.pdf"
+        print(filename)
+        f.savefig(filename, pad_inches = 10)
 
 if __name__ == "__main__":
     main()
